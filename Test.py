b67e84f4600f1148acb56a203b36c50055fcea5a
user.email <User@thomasbasoglu>
2021-09-29 13:02:12 +0300

    updated

--- Test.py
+++ Test.py
@@ -1,1 +1,17 @@
-print('Hello')
+from decimal import Decimal
+import decimal
+def kdv():
+    price = float(input('Fiyatı ekle: '))
+    kdv = int(input('kdv oranını ekle: '))
+    kdvVals = [1,8,10,18]
+    
+    if kdv in kdvVals:
+        kdvVal = price * kdv / 100 
+        mat = price - kdvVal 
+        print(price)
+        print(mat)      
+    else:
+        print('Vergiyi doğru yazdığınıza emin olun')
+
+while True:
+    kdv()
