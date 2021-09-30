26634c996f76db48c370e9a2935fcfceee8b53f1
user.email <User@thomasbasoglu>
2021-09-30 16:24:40 +0300

    minor changes on vergi

--- Vergi.py
+++ Vergi.py
@@ -4,23 +4,26 @@ class Error(Exception):
 class vergiError(Error):
     pass
 
-def kdv(price, kdv):
-    
+def kdv():
+    price = float(input('Fiyatı ekle: '))
+    kdv = int(input('kdv oranını ekle: '))
     kdvVals = [1,8,10,18]
     try:    
         if int(kdv) in kdvVals:
             kdvVal = price * kdv / 100 
             mat = price - kdvVal
-            return mat      
+            
+                  
         else:
             raise vergiError
     
     except vergiError:
-        return 'Vergiyi doğru yazdığınıza emin olun'
+        print('Vergiyi doğru yazdığınıza emin olun')
+
+def toString():
+    kdv.price
+ 
 
         
 
-while True:
-    price = float(input('Fiyatı ekle: '))
-    kdv = input('kdv oranını ekle: ')
-    kdv(price, kdv)
+
