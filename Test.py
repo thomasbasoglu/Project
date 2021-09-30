2eb101cd6ce578e8c5c0d9804c6e4436ab894892
user.email <User@thomasbasoglu>
2021-09-30 14:51:17 +0300

    added exceptionc to vergi

--- Test.py
+++ Test.py
@@ -1,15 +1,26 @@
+class Error(Exception):
+    pass
+
+class vergiError(Error):
+    pass
+
 def kdv():
     price = float(input('Fiyatı ekle: '))
     kdv = int(input('kdv oranını ekle: '))
     kdvVals = [1,8,10,18]
+    try:    
+        if kdv in kdvVals:
+            kdvVal = price * kdv / 100 
+            mat = price - kdvVal 
+            print(price)
+            print(mat)      
+        else:
+            raise vergiError
     
-    if kdv in kdvVals:
-        kdvVal = price * kdv / 100 
-        mat = price - kdvVal 
-        print(price)
-        print(mat)      
-    else:
+    except vergiError:
         print('Vergiyi doğru yazdığınıza emin olun')
+
+        
 
 while True:
     kdv()
