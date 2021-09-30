84af084fcdbfa5610569fe6d93c0af06092e7d30
user.email <User@thomasbasoglu>
2021-09-30 16:45:49 +0300

    changes according product

--- Vergi.py
+++ Vergi.py
@@ -4,26 +4,28 @@ class Error(Exception):
 class vergiError(Error):
     pass
 
-def kdv():
-    price = float(input('Fiyatı ekle: '))
-    kdv = int(input('kdv oranını ekle: '))
-    kdvVals = [1,8,10,18]
-    try:    
-        if int(kdv) in kdvVals:
-            kdvVal = price * kdv / 100 
-            mat = price - kdvVal
-            
-                  
-        else:
-            raise vergiError
-    
-    except vergiError:
-        print('Vergiyi doğru yazdığınıza emin olun')
+class Product:
+    def __init__(self, price, kdv):
+        self.price = price
+        self.kdv = kdv
 
-def toString():
-    kdv.price
- 
+    def kdv(price, kdv):
+        try:    
+            kdvVals = [1,8,10,18]    
+            if kdv in kdvVals:
+                kdvVal = price * kdv / 100 
+                mat = price - kdvVal 
+                return mat,kdvVal              
+            else:
+                raise vergiError
+        except vergiError:
+            print('Vergiyi doğru yazdığınıza emin olun')
 
-        
+price = float(input('Fiyatı ekle: '))
+kdv = int(input('kdv oranını ekle: '))
+
+Product(price, kdv)
+
+
 
 
