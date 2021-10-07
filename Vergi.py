057d12631ec91e323f4ad9148e5203e8192190d7
user.email <User@thomasbasoglu>
2021-10-07 11:25:22 +0300

    launch json and to string updates

--- Vergi.py
+++ Vergi.py
@@ -7,29 +7,48 @@ class vergiError(Error):
 class Product:
     def __init__(self, price, kdv):
         self.price = price
-        self.kdv = kdv
-
-
-    def kdv(price, kdv):
+        self.kdv = kdv 
+        
         try:    
             kdvVals = [1,8,10,18]    
             if kdv in kdvVals:
                 kdvVal = price * kdv / 100 
                 mat = price - kdvVal 
-                return mat,kdvVal              
+                self.mat = mat
+                self.kdvVal = kdvVal
+
             else:
-                raise vergiError
+                mat = None
+                self.mat = mat
+        
         except vergiError:
             print('Vergiyi doğru yazdığınıza emin olun')
+        
     def toString(self):
-        print('Fiyatı ' + str(self.price) + ' olan ve ' + str(self.kdv) 
-    + ' kdv ye tabi tutulan ürünün \n' + 'matrahı: ' + str(kdv(self.price,self.kdv)))
+        print('Fiyatı ' + str(self.price) + ' olan ve ' + str(self.kdv)
+            + ' kdv ye tabi tutulan ürünün \n' + 'matrahı: ' + str(self.mat) + 
+                'TL\nkesilen kdv tutarı ' + str(self.kdvVal) + 'TL')    
+
+def KKEG():
+    print(Product.mat)
 
 
 price = float(input('Fiyatı ekle: '))
-kdv = int(input('kdv oranını ekle: '))
+kdv = input('kdv oranını ekle: ')
 
 Product(price, kdv).toString()
+
+type = input('\nBinek araç ise 1e, ticari araç ise 2ye, hiçbiri değil ise 3e basınız: ')
+
+if type == 1:
+    KKEG()
+
+if type == 2:
+    print(2)
+
+if type == 3:
+    print(3)
+    
 
 
 
