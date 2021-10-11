5306220b34a43abe2e42eb71d44775ec0a792fed
user.email <User@thomasbasoglu>
2021-10-11 15:14:11 +0300

    fixed the problem with kdvVal

--- Vergi.py
+++ Vergi.py
@@ -4,8 +4,9 @@ class Product:
     def __init__(self, price, kdv):
         self.price = price
         self.kdv = kdv      
-        self.kdvVal = price * int(kdv) / 100 
-        self.mat = price - self.kdvVal 
+        self.mat = price / (1 + int(kdv)/100) #1.18 çarp 
+        self.kdvVal = price - self.mat 
+        
     
 
         
@@ -13,17 +14,19 @@ class Product:
         
     def toString(self):
         print('Fiyatı ' + str(self.price) + ' olan ve ' + str(self.kdv)
-            + ' kdv ye tabi tutulan ürünün \n' + 'matrahı: ' + str(self.mat) + 
-                'TL\nkesilen kdv tutarı ' + str(self.kdvVal) + 'TL')    
+            + ' kdv ye tabi tutulan ürünün \n' + 'matrahı: ' + str(round(self.mat*100)/100) + 
+                'TL\nkesilen kdv tutarı ' + str(round(self.kdvVal*100)/100) + 'TL')    
 
     def KKEG(self):
-        mat30 = self.mat*30/100
-        mat70 = self.mat*70/100
-        kdv30 = self.kdvVal*30/100
-        kdv70 = self.kdvVal*70/100
+        mat30 = round(self.mat*30/100*100)/100
+        mat70 = round(self.mat*70/100*100)/100
+        kdv30 = round(self.kdvVal*30/100*100)/100
+        kdv70 = round(self.kdvVal*70/100*100)/100
         
         KKEG = mat30 + kdv30
-        table = [['Toplam', self.price*70/100, self.price*30/100], ['Matrah', mat70, mat30], ['KDV', kdv70, kdv30]]
+        table = [[' ', 'Toplam', 70, 30],['Toplam', price, self.price*70/100, self.price*30/100], 
+                      ['Matrah', round(self.mat*100)/100, mat70, mat30], 
+                      ['KDV',round(self.kdvVal*100)/100, kdv70, kdv30]]
     
         print(tabulate(table, tablefmt='fancy_grid'))  
         print('Matrah70: ' + str(mat70))
@@ -33,22 +36,23 @@ class Product:
         print('Mat30: ' + str(mat30))
         print('KDV30: ' + str(kdv30))
         
+while True:
+        
+    price = float(input('Fiyatı ekle: '))
+    kdv = input('kdv oranını ekle: ')
+    product = Product(price, kdv)    
+    product.toString()
+    type = input('\nBinek araç ise 1e, ticari araç ise 2ye, hiçbiri değil ise 3e basınız: ')
 
-price = float(input('Fiyatı ekle: '))
-kdv = input('kdv oranını ekle: ')
-product = Product(price, kdv)    
-product.toString()
-type = input('\nBinek araç ise 1e, ticari araç ise 2ye, hiçbiri değil ise 3e basınız: ')
+    if int(type) == 1:
+        product.KKEG()
 
-if int(type) == 1:
-    product.KKEG()
+    if type == 2:
+        print(2)
 
-if type == 2:
-    print(2)
-
-if type == 3:
-    print(3)
-                
+    if type == 3:
+        print(3)
+                    
 
 
 
